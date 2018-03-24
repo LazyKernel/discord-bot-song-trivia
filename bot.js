d66b7865be80cecb608a5bf1dc73afff3bcc2801
Haoyang <hygzhu@gmail.com>
2018-03-24 10:35:56 -0400

    Now checks for corect answer

--- bot.js
+++ bot.js
@@ -3,10 +3,9 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const trivia = require("./trivia.js");
 
-
 //on startup
 client.on("ready", () => {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} Servers.`); 
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} Servers.`);
   client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
@@ -26,43 +25,47 @@ client.on("guildDelete", guild => {
 // This event will run on every single message received, from any channel or DM.
 client.on("message", async message => {
 
-  console.log(`Message in Server: ${message.guild} in ${message.channel.id}`);
- 
   //Message does not work in servers so 
   if (!message.guild) return;
   //Ignores itself to prevent spam loop
-  if(message.author.bot) return;
+  if (message.author.bot) return;
   // ignore any message that does not start with our prefix
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
   //ignore any message not in the #anime-son-trivia or bot-test text channel
-  if(message.channel.id != "426862381025591317") return;
+  if (message.channel.id != "426862381025591317") return;
 
   // Seperate commands into args
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if(command === "ping") {
+  if (command === "reset") {
+    message.channel.send('Resetting...')
+      .then(message => client.destroy())
+      .then(() => {client.login(config.token)});
+  }
+
+  if (command === "ping") {
     // Latency check
     //Round trip latency, average oneway websocket server latency
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
-  
-  if(command === "cmd") {
-    let sayMessage = "Your command was: "+ args.join(" ");
+
+  if (command === "cmd") {
+    let sayMessage = "Your command was: " + args.join(" ");
     // deletes the command message and handles exception
-    message.delete().catch(O_o=>{}); 
+    message.delete().catch(O_o => { });
     // Repeats the command
     message.channel.send(sayMessage);
   }
 
   //Adds the bot to the voice channel of whoever called it (Needs to have role mod)
-  if(command === "init" || message.member.roles.some(r=>["Mod"].includes(r.name))) {
+  if (command === "init" || message.member.roles.some(r => ["Mod"].includes(r.name))) {
     //join the channel of whoever called this command
     trivia.join(message);
   }
 
-  if(command == "play"){
+  if (command == "play") {
     trivia.play(message);
   }
 });
--- trivia.js
+++ trivia.js
@@ -1,88 +1,120 @@
 const config = require('./config.json');
 var animelist = require('./animelist.json');
 
+let answers = [];
 let playing = false;
 
-//gets a new trivia round
-function getTrivia(optionsCount) {
-    //gets new anime
-    const total = animelist.length -1;
-    const randAnime = Math.floor((Math.random() * total) + 1);
-    const newAnime = animelist[randAnime];
-    const newAnimeName = newAnime["source"];
-  
-    //gets random options
-    let randomOptions = [];
-    for (let i = 0; i < optionsCount; i++) {
-      let index = Math.floor((Math.random() * total) + 1);
-      let animeName = animelist[index]["source"];
-      //prevents duplicates
-      while (newAnimeName == animeName || randomOptions.includes(animeName)) {
-        index = Math.floor((Math.random() * total) + 1);
-        animeName = animelist[index]["source"];
-      }
-      randomOptions.push(animeName);
-    }
-    randomOptions[Math.floor((Math.random() * optionsCount))] = newAnimeName;
-  
-    data = {
-      "animeName": newAnime["source"],
-      "title": newAnime["title"],
-      "songName": newAnime["song"] ? newAnime["song"]["title"] : null,
-      "songArtist": newAnime["song"] ? newAnime["song"]["artist"] : null,
-      "filename": "./to_ignore/songs/" + animelist[randAnime]["file"] + ".mp3",
-      "options": randomOptions,
-    };
-  
-    return data;
+//gets a list of anime names to check answers with
+function getAnswers() {
+  const total = animelist.length - 1;
+  for (let i = 0; i < total; i++) {
+    answers.push(animelist[i]["source"].toLowerCase());
   }
+  //remove dupes with dedup
+  answers = answers.filter((item, i, arr) => arr.indexOf(item) === i);
+}
+
+//gets a new trivia round
+function getTrivia() {
+  //gets new anime
+  const total = animelist.length - 1;
+  const randAnime = Math.floor((Math.random() * total) + 1);
+  //prevent null
+  while (animelist[randAnime] == undefined) {
+    randAnime = Math.floor((Math.random() * total) + 1);
+  }
+  const newAnime = animelist[randAnime];
+  const newAnimeName = newAnime["source"];
+
+  data = {
+    "animeName": newAnime["source"],
+    "title": newAnime["title"],
+    "filename": animelist[randAnime]["file"],
+  };
+
+  return data;
+}
 
 const commands = {
-	'play': (msg) => {
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-		if (playing) return msg.channel.sendMessage('Already Playing');
-		let dispatcher;
-        playing = true;
-        
-		(function play(song) {
-            let trivia = getTrivia(4);
-            dispatcher =  msg.guild.voiceConnection.playFile(trivia.filename);
-            msg.channel.send('New song! Which one is it\n\n\n' + trivia.options.map((x, i)=> ` ${i+1}: [${x}] `));
-            //Creates a collection of commands for the channel
-            let collector = msg.channel.createCollector(m => m);
-            collector.on('collect', m => {
-              let isMod = m.member.roles.some(r=>["Mod"].includes(r.name));  
-              if (m.content.startsWith(config.prefix + 'pause') && isMod) {
-                msg.channel.send('paused').then(() => {dispatcher.pause();});
-              } else if (m.content.startsWith(config.prefix + 'resume') && isMod){
-                msg.channel.send('resumed').then(() => {dispatcher.resume();});
-              }else if (m.content.startsWith(config.prefix + 'time') && isMod){
-                msg.channel.send(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
-              }else if(m.content.startsWith(config.prefix + 'stop')&& isMod){
-                msg.channel.send('stopped').then(() => {dispatcher.end();});
-              }else if(m.content == (config.prefix + 'answer')){
-                msg.channel.send('Submitted answer!');
-              }
-			});
-			dispatcher.on('end', () => {
-				collector.stop();
-				play("");
-			});
-			dispatcher.on('error', (err) => {
-				return msg.channel.send('error: ' + err).then(() => {
-					collector.stop();
-					play("");
-				});
-			});
-		})("");
-	},
-	'join': (msg) => {
-		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('Could not connect to voice channel');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-    }
+  'play': (msg) => {
+    if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
+    if (playing) return msg.channel.send('Already Playing');
+    let dispatcher;
+    playing = true;
+    getAnswers();
+
+    (function play(song) {
+      let trivia = getTrivia();
+      console.log(trivia)
+      //dispatcher = msg.guild.voiceConnection.playFile("./to_ignore/songs/" + trivia.filename + ".mp3");
+      dispatcher = msg.guild.voiceConnection.playArbitraryInput("http://openings.moe/video/" + trivia.filename);
+      msg.channel.send("New song! Make a guess!");
+      console.log("Song is from :" + trivia.animeName);
+      console.log(msg.guild.voiceConnection.status);
+      //msg.channel.send('New song! Which one is it\n\n\n' + trivia.options.map((x, i)=> ` ${i+1}: [${x}] `));
+      //Creates a collection of commands for the channel
+      let collector = msg.channel.createCollector(m => m);
+      collector.on('collect', m => {
+        let isMod = m.member.roles.some(r => ["Mod"].includes(r.name));
+        if (m.content.startsWith(config.prefix + 'pause') && isMod) {
+          msg.channel.send('paused').then(() => { dispatcher.pause(); });
+        } else if (m.content.startsWith(config.prefix + 'resume') && isMod) {
+          msg.channel.send('resumed').then(() => { dispatcher.resume(); });
+        } else if (m.content.startsWith(config.prefix + 'time') && isMod) {
+          console.log(msg.guild.voiceConnection);
+          msg.channel.send(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000) / 1000) < 10 ? '0' + Math.floor((dispatcher.time % 60000) / 1000) : Math.floor((dispatcher.time % 60000) / 1000)}`);
+        } else if (m.content.startsWith(config.prefix + 'song') && isMod) {
+          msg.channel.send(`The song is from ${trivia.animeName}`);
+        } else if (m.content.startsWith(config.prefix + 'file') && isMod) {
+          msg.channel.send(`The song is from ${trivia.filename}`);
+        } else if (m.content.startsWith(config.prefix + 'skip') && isMod) {
+          msg.channel.send('skipping').then(() => { dispatcher.end(); });
+        } else if (m.content.startsWith(config.prefix + 'end') && isMod) {
+          playing = false;
+          msg.channel.send('Ending').then(() => { dispatcher.end(); });
+        } else if (m.content.startsWith(config.prefix + 'a ')) {
+          msg.channel.send('Submitted answer!');
+          //parse args
+          const query = m.content.slice(config.prefix.length + 1).trim();
+          console.log(query);
+          //at least 4 chars long
+          if(query.length >= 4){
+            let pattern = new RegExp(query);
+            let matches = answers.filter(x => x.match(pattern));
+            console.log(matches);
+            console.log(trivia.animeName);
+            if(matches.includes(trivia.animeName.toLowerCase())){
+              msg.channel.send('You got it right! The anime is called: ' + trivia.animeName );
+            }
+          }
+        }
+      });
+      dispatcher.on('end', () => {
+        collector.stop();
+        if (playing) {
+          play("");
+        }
+      });
+      dispatcher.on('error', (err) => {
+        return msg.channel.send('error: ' + err).then(() => {
+          console.log(err);
+          playing = false;
+          collector.stop();
+          dispatcher.end();
+        });
+      });
+    })("");
+  },
+  'join': (msg) => {
+    return new Promise((resolve, reject) => {
+      const voiceChannel = msg.member.voiceChannel;
+      if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('You have to be in a voice channel!');
+      voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+    });
+  },
+  'reset': () => {
+    playing = false;
+  }
 };
 
-module.exports = commands ; 
+module.exports = commands; 
